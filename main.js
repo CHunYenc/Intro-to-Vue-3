d8a1f03c3c3dd357b537f84248c9b99b7fab1c13
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2021-12-12 11:54:06 +0800

    feat: Finish L2 Challenge.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
+      description:
+        "SOCKS is an Internet protocol that exchanges network packets between a client and server through a proxy server.",
     };
   },
 });
